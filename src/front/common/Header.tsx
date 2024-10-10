644ab9533487e0df8a5e261b929d061ba2fd0f6c
Andrius Simanaitis <eragonas5@gmail.com>
2024-10-10 13:01:53 +0300

    add language switching functionality in header

--- src/front/common/Header.tsx
+++ src/front/common/Header.tsx
@@ -6,13 +6,33 @@ export default function Header() {
     <header className="Common__Header">
       <div className="Common__Header__Logo">Kalbyn[ɐ̝]s</div>
       <div className="Common__Header__Languages">
-        <a href="/" title="lietuvių k.">
+        <a
+          href={getNewUrl('lt')}
+          title="lietuvių k."
+          rel="alternate"
+          hrefLang="lt"
+        >
           <img src="/static/lt.png" alt="lietuvių k." />
         </a>
-        <a href="/" title="English">
+        <a href={getNewUrl('en')} title="English" rel="alternate" hrefLang="en">
           <img src="/static/en.png" alt="English" />
         </a>
       </div>
     </header>
   )
+}
+
+function getNewUrl(subdomain: string) {
+  const DEFAULT_LOCALE = 'lt'
+  const splitHostName = window.location.hostname.split('.')
+  let domain: string
+  if (splitHostName.length === 2) {
+    domain = window.location.hostname
+  } else {
+    domain = `${splitHostName[1]}.${splitHostName[2]}`
+  }
+  const sub = subdomain === DEFAULT_LOCALE ? '' : `${subdomain}.`
+  const protocol = `${window.location.protocol}//`
+  const port = window.location.port === '' ? '' : `:${window.location.port}`
+  return protocol + sub + domain + port + window.location.pathname
 }
