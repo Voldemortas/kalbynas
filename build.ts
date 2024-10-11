6122b146f6460462254d03b25909934d3fed2ea5
Andrius Simanaitis <eragonas5@gmail.com>
2024-10-11 09:54:02 +0300

    fix building errors

--- build.ts
+++ build.ts
@@ -1,18 +1,19 @@
-import { $ } from "bun";
-import buildFront from "./build-front";
+import {$} from 'bun'
+import buildFront from './build-front'
 import pages from './src/pages'
 import type {PageType, ReactType} from './src/pages'
-import buildBack from "./build-back";
+import buildBack from './build-back'
 
 export default async function build() {
-    await $`rm -rf out`
-    await $`mkdir out`
-    await $`cp -r src/static/ out/static/`
-    const frontPaths = (pages as PageType<ReactType>[])
-        .filter(p => p.resolve.type === "react")
-        .map(p => p.resolve.path)
-    await buildFront(frontPaths) 
-    await buildBack()
+  await $`rm -rf out`
+  await $`rm -rf temp`
+  await $`mkdir out`
+  await $`cp -r src/static/ out/static/`
+  const frontPaths = (pages as PageType<ReactType>[])
+    .filter((p) => p.resolve.type === 'react')
+    .map((p) => p.resolve.path)
+  await buildFront(frontPaths)
+  await buildBack()
 }
 
 await build()
