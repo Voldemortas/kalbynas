a93f0cd88bb652157c8cb5769f0c7eea1e82e60b
Andrius Simanaitis <eragonas5@gmail.com>
2025-06-29 05:32:58 +0300

    fix showing Lithuanian article titles on English locale

--- src/back/pages/articles/index.ts
+++ src/back/pages/articles/index.ts
@@ -1,15 +1,17 @@
 import translations from 'back/translations/articles'
 import getPageWithAllTranslations from 'back/pages/common/getPageWithAllTranslations.ts'
 import articleList from 'back/pages/articles/allArticles'
+import getLocale from 'back/common/getLocale.ts'
 
 const NAV_LINK = '/articles'
 
 export default function index(request: Request, params: string[]) {
+  const locale = getLocale(request)
   return {
     ...getPageWithAllTranslations(request, NAV_LINK, translations),
     articleList: articleList.map((article, id) => ({
       id: article.id.format(),
-      title: article.title.format(),
+      title: article.title.format(locale),
     })),
   }
 }
