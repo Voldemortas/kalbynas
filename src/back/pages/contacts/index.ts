2b7adae8f48158e4d5e69c3225fd8d583c926df6
Andrius Simanaitis <eragonas5@gmail.com>
2025-09-25 23:48:21 +0300

    update discord link

--- src/back/pages/contacts/index.ts
+++ src/back/pages/contacts/index.ts
@@ -2,7 +2,7 @@ import translations from 'back/translations/contact'
 import getPageWithAllTranslations from 'back/pages/common/getPageWithAllTranslations.ts'
 
 const NAV_LINK = 'CONTACTS'
-const DISCORD = 'discord://-users/184706446477361153'
+const DISCORD = 'https://discordapp.com/users/184706446477361153'
 const EMAIL = 'contact@kalbynas.lt'
 
 export default function index(request: Request, params: string[]) {
